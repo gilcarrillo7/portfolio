f882c9bd42299a3541e09d1b17f5dafb7fe937d4
gilbertotv <gilcarrillo7@gmail.com>
2023-05-02 00:27:02 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": ""
+    }
+  }, "gatsby-plugin-react-helmet"]
+};
